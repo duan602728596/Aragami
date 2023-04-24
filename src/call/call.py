eec893a72d3b16928abbc1c00fdf53bcc1547aa2
段昊辰 <duanhaochen@126.com>
2023-04-24 23:26:29 +0800

    perf: 优化代码

--- src/call/call.py
+++ src/call/call.py
@@ -2,12 +2,17 @@ from src.api.api import request_ttwid_cookie, request_detail
 from src.api.utils import random_string
 
 
-def detail(id: str):
+# 生成cookie
+def get_cookie() -> str:
     passport_csrf_token: str = random_string(32)
     ttwid: str = request_ttwid_cookie()
-    cookie = '; '.join((
+
+    return '; '.join((
         ttwid,
         'passport_csrf_token=' + passport_csrf_token,
         'passport_csrf_token_default=' + passport_csrf_token
     ))
-    return request_detail(id, cookie)
+
+
+def detail(id: str):
+    return request_detail(id, get_cookie())
